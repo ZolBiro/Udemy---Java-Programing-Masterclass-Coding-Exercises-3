7a985005358a2fb845c38fbbc445f215b9254984
Zoltan Biro <36306066+ZolBiro@users.noreply.github.com>
2019-04-07 10:59:41 +0200

    Add files via upload

---
+++ BarkingDog/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean barkingDog = BarkingDog.shouldWakeUp(false,2);
+        System.out.println(barkingDog);
+    }
+}
